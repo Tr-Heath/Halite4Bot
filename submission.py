4745677f45e2872df13d834075ef440af6ce6098
Travis Heath <travis.heath@outlook.com>
2020-10-08 22:27:31 -0700

    choice update

--- submission.py
+++ submission.py
@@ -1,12 +1,14 @@
 #Starting submission for Halite 4 comp.
 from kaggle_environments.envs.halite.helpers import *
+from random import choice
+
 def agent(obs, config):
     board = Board(obs, config)
     me = board.current_player
     
     #Set actions for each ship
     for ship in me.ships:
-        ship.next_action = ShipAction.NORTH
+        ship.next_action = choice([ShipAction.NORTH, ShipAction.EAST, ShipAction.SOUTH, ShipAction.WEST, None])
 
     #Set actions for each shipyard
     for shipyard in me.shipyards:
